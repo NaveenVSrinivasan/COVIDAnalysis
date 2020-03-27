6aa07b3e857e7a0257f4dae30d5a452371f1a6dd
Emily Mu <emilymu@Emilys-MacBook-Pro-2.local>
2020-03-27 16:57:58 -0500

    Added document extraction function

--- clustering/text_extraction_functions.py
+++ clustering/text_extraction_functions.py
@@ -1,6 +1,7 @@
 import glob
 import json
 import nltk.data
+import re
 from transformers import *
 
 
@@ -50,9 +51,51 @@ def extract_abstracts(directory, remove_ints=False, use_titles=True):
     return abstracts
 
 
-def extract_body_text(directory, remove_ints=False, use_abstracts=True):
-    pass
+def extract_documents(directory, remove_ints=False, use_abstracts=True):
+    """
+    :param directory: directory to recursively find json files
+    :param remove_ints: optional argument to remove digits and websites
+    :param use_abstracts: if abstracts (and titles) included
+    :return: list of documents which are lists of paragraphs which are lists of sentences
+    """
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    documents = []
 
+    for file in sorted(list(glob.glob(directory+'**/*.json', recursive=True))):
+        paper_data = json.load(open(file))               # <-- Things done for each file
+        paragraphs = []
+        if use_abstracts:
+            if len(paper_data['abstract']) > 0:
+                all_text = " ".join([x["text"] for x in paper_data['abstract']])
+                all_text = paper_data['metadata']['title']+". " + all_text
+                all_text = all_text.replace("All rights reserved. No reuse allowed without permission.","")
+                all_text = all_text.replace("Abstract", "")
 
-def extract_all_paragraphs(directory, remove_ints=False):
-    pass
+                sentences = [s for s in sent_detector.tokenize(all_text.strip()) if "author/funder." not in s and len(s.split()) > 1]
+
+                if remove_ints:
+                    clean = lambda x: " ".join([word for word in re.split("\W+", x) if not word.isdigit() and "http" not in word])
+                    sentences = [clean(s) for s in sentences]
+
+                if len(sentences) > 0:
+                    paragraphs.append(sentences)
+
+        for paragraph in paper_data['body_text']:
+            all_text = paragraph['text']
+
+            all_text = all_text.replace("All rights reserved. No reuse allowed without permission.", "")
+            all_text = all_text.replace("author/funder", "")
+
+            sentences = [s for s in sent_detector.tokenize(all_text.strip()) if 'copyright' not in s and
+                         'medRxiv' not in s and len(s.split()) > 1]
+
+            if remove_ints:
+                clean = lambda x: " ".join([word for word in re.split("\W+", x) if not word.isdigit() and "http" not in word])
+                sentences = [clean(s) for s in sentences]
+
+            if len(sentences) > 0:
+                paragraphs.append(sentences)
+
+        documents.append(paragraphs)
+
+    return documents
