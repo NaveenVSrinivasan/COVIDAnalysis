fd1beb9ce3af6b6ab19dc9dca258c48c311c3e9a
Zachary Horvitz <zacharyhorvitz@users.noreply.github.com>
2020-04-05 22:00:17 -0400

    added mesh text

--- mesh/parsh_mesh_tags.py
+++ mesh/parsh_mesh_tags.py
@@ -12,6 +12,18 @@ for child in root:
 		if info.tag == "DescriptorName":
 			all_text.extend(info[0].text.lower().replace(",","").split())
 
-for t in sorted(list(set(all_text))):
+clean_text = []
+
+for t in all_text:
+	clean_text.append(t)
+	clean_text.append(t.replace("(","").replace(")",""))
+	clean_text.extend(t.split("-"))
+	clean_text.extend(t.replace("(","").replace(")","").split("-"))
+
+
+clean_text = [x for x in clean_text if len(t) > 1 and not t.isdigit()]
+
+
+for t in sorted(list(set(clean_text))):
 	print(t)
 	
