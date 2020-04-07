47ec73f2583e6a2f6e9e3229bfe73388a79e440e
Emily Mu <emilymu@Emilys-MacBook-Pro-2.local>
2020-04-07 10:10:50 -0500

    Added try/except clause

--- clustering/build_embedding_functions.py
+++ clustering/build_embedding_functions.py
@@ -55,17 +55,22 @@ def build_scibert_embeds_paragraphs(paragraphs):
 
     ######Build Vectors######################
     vector_representation = []
+    paragraphs_ = []
     for a in tqdm(paragraphs):
-        all_sentence_embeds = []
-        # print(a)
-        for sentence in a:
-            tokenized = torch.tensor([tokenizer.encode(sentence)]) #.cuda()
-            hidden_states,cls_emb = model(tokenized)
-            all_sentence_embeds.append(torch.sum(hidden_states,1).view(-1))
-        abstract_embeddings = torch.stack(all_sentence_embeds)
-        vector_representation.append(torch.sum(abstract_embeddings,axis=0).data.numpy())
+        try:
+            all_sentence_embeds = []
+            # print(a)
+            for sentence in a:
+                tokenized = torch.tensor([tokenizer.encode(sentence)]) #.cuda()
+                hidden_states,cls_emb = model(tokenized)
+                all_sentence_embeds.append(torch.sum(hidden_states,1).view(-1))
+            abstract_embeddings = torch.stack(all_sentence_embeds)
+            vector_representation.append(torch.sum(abstract_embeddings,axis=0).data.numpy())
+            paragraphs_.append(a)
+        except:
+            print(a)
 
-    text_to_embeddings = {" ".join(a): v for a, v in zip(paragraphs, vector_representation)}
+    text_to_embeddings = {" ".join(a): v for a, v in zip(paragraphs_, vector_representation)}
 
     return text_to_embeddings
 
@@ -107,35 +112,42 @@ def build_scibert_embeds_tfidf_paragraphs(paragraphs):
     vectorizer = TfidfVectorizer()
 
     corpus = [" ".join([" ".join(tokenizer.tokenize(s)) for s in a]) for a in paragraphs]
-    all_sentence_embeds = []
+    vector_representation = []
+    paragraphs_ = []
     X = vectorizer.fit_transform(corpus)
-    words_to_index = {w:i for i,w in enumerate(vectorizer.get_feature_names())}  
+    words_to_index = {w:i for i,w in enumerate(vectorizer.get_feature_names())}
     with tqdm(total=len(paragraphs)) as pbar:
         for tfidf_score,text in tqdm(zip(X,paragraphs)):
-            tfidf_score = tfidf_score.toarray()
-            for sentence in text:
-                tokenized = tokenizer.tokenize(sentence)
-                weights = []
-                for w in tokenized:
-                    if w in words_to_index:
-                        weights.append(tfidf_score[0][words_to_index[w]])
-                    else:
-                        weights.append(0.0)
+            try:
+                print(text)
+                all_sentence_embeds = []
+                tfidf_score = tfidf_score.toarray()
+                for sentence in text:
+                    tokenized = tokenizer.tokenize(sentence)
+                    weights = []
+                    for w in tokenized:
+                        if w in words_to_index:
+                            weights.append(tfidf_score[0][words_to_index[w]])
+                        else:
+                            weights.append(0.0)
 
-                weights = torch.tensor(weights)
-                weights = weights / torch.sum(weights) if torch.sum(weights) > 0 else weights
+                    weights = torch.tensor(weights)
+                    weights = weights / torch.sum(weights) if torch.sum(weights) > 0 else weights
 
-                # print(weights)
+                    # print(weights)
 
-                hidden_states,cls_emb = model(torch.tensor([tokenizer.encode(sentence,add_special_tokens=False)]))
-                # print(hidden_states.size())
-                # print(weights.size())
+                    hidden_states,cls_emb = model(torch.tensor([tokenizer.encode(sentence,add_special_tokens=False)]))
+                    # print(hidden_states.size())
+                    # print(weights.size())
 
-                new_hidden_states = torch.mm(hidden_states.permute((0,2,1)).squeeze(0),weights.reshape(-1,1))
-                all_sentence_embeds.append(new_hidden_states.view(-1).data.numpy())
-            pbar.update(1)
+                    new_hidden_states = torch.mm(hidden_states.permute((0,2,1)).squeeze(0),weights.reshape(-1,1))
+                    all_sentence_embeds.append(new_hidden_states.view(-1).data.numpy())
+                paragraphs_.append(text)
+                pbar.update(1)
+            except:
+                print(text)
 
-    text_to_embeddings = {" ".join(a):v for a,v in zip(paragraphs, all_sentence_embeds)}
+    text_to_embeddings = {" ".join(a):v for a,v in zip(paragraphs_, all_sentence_embeds)}
 
     return text_to_embeddings
 
@@ -154,36 +166,40 @@ def build_scibert_embeds_mesh_paragraphs(paragraphs,text_features):
     # vectorizer = TfidfVectorizer()
 
     corpus = [" ".join([" ".join(tokenizer.tokenize(s)) for s in a]) for a in paragraphs]
-    all_sentence_embeds = []
-    # X = vectorizer.fit_transform(corpus)
-    # words_to_index = {w:i for i,w in enumerate(vectorizer.get_feature_names())}  
+    vector_representation = []
+    paragraphs_ = []
 
     with tqdm(total=len(paragraphs)) as pbar:
         for text in tqdm(paragraphs):
-            for sentence in text:
-                tokenized = tokenizer.tokenize(sentence)
-                weights = []
-                for w in tokenized:
-                    # print(w)
-                    if w in text_features:
-                        # print("\tFOUND")
-                        weights.append(1.0) #tfidf_score[0][words_to_index[w]])
-                    else:
-                        weights.append(0.0)
+            try:
+                all_sentence_embeds = []
+                for sentence in text:
+                    tokenized = tokenizer.tokenize(sentence)
+                    weights = []
+                    for w in tokenized:
+                        # print(w)
+                        if w in text_features:
+                            # print("\tFOUND")
+                            weights.append(1.0) #tfidf_score[0][words_to_index[w]])
+                        else:
+                            weights.append(0.0)
 
-                weights = torch.tensor(weights)
-                # weights = weights / torch.sum(weights) if torch.sum(weights) > 0 else weights
+                    weights = torch.tensor(weights)
+                    # weights = weights / torch.sum(weights) if torch.sum(weights) > 0 else weights
 
-                # print(weights)
+                    # print(weights)
 
-                hidden_states,cls_emb = model(torch.tensor([tokenizer.encode(sentence,add_special_tokens=False)]))
-                # print(hidden_states.size())
-                # print(weights.size())
+                    hidden_states,cls_emb = model(torch.tensor([tokenizer.encode(sentence,add_special_tokens=False)]))
+                    # print(hidden_states.size())
+                    # print(weights.size())
 
-                new_hidden_states = torch.mm(hidden_states.permute((0,2,1)).squeeze(0),weights.reshape(-1,1))
-                all_sentence_embeds.append(new_hidden_states.view(-1).data.numpy())
-            pbar.update(1)
+                    new_hidden_states = torch.mm(hidden_states.permute((0,2,1)).squeeze(0),weights.reshape(-1,1))
+                    all_sentence_embeds.append(new_hidden_states.view(-1).data.numpy())
+                paragraphs_.append(text)
+                pbar.update(1)
+            except:
+                print(text)
 
-    text_to_embeddings = {" ".join(a):v for a,v in zip(paragraphs, all_sentence_embeds)}
+    text_to_embeddings = {" ".join(a):v for a,v in zip(paragraphs_, vector_representation)}
 
     return text_to_embeddings
